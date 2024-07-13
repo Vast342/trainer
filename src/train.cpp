6a1b280c4fadd47e4c8aa620a26f0d439549a117
Vast <vast342e@gmail.com>
2024-07-13 18:43:39 -0400

    sigmoid

--- src/train.cpp
+++ src/train.cpp
@@ -20,7 +20,7 @@ void train([[maybe_unused]]Network &network) {
                 // stuff it into network
                 NetworkState state = NetworkState(&network, entry);
                 // inference
-                const float eval = state.evaluate();
+                const float eval = sigmoid(state.evaluate(), 1);
                 const float currentWDL = std::lerp(wdlStart, wdlEnd, double(superBatch) / double(endSB));
                 const float expected = std::lerp(sigmoid(entry.score, evalScale), entry.result, currentWDL);
                 const float error = std::pow(eval - expected, 2); 
